847f149004f154f1d636fc77984e15865ec9aa7e
EJ <me@me>
2021-10-20 19:53:29 -0400

    skills

--- src/Tech.svelte
+++ src/Tech.svelte
@@ -25,19 +25,19 @@
 <SectionHeader>Tech</SectionHeader>
 <div class="bars">
     <SkillBar
-        percentage={95}
+        percentage={98}
         tools={['Flask', 'Django', 'PyUnit', 'Extensions API']}>
         Python
     </SkillBar>
     <!-- TODO need better java skills these kinda suckzzz -->
-    <SkillBar percentage={97} tools={['Spring', 'Gradle', 'Hibernate', 'Kotlin', 'Scala']}>Java</SkillBar>
-    <SkillBar percentage={97} tools={['ASP.NET', 'EntityFramework', 'C#', 'Mono']}>.NET</SkillBar>
-    <SkillBar percentage={97} tools={['TypeScript', 'React', 'Angular', 'Sass', 'Svelte', 'Electron']}>Front-End</SkillBar>
-    <SkillBar percentage={97} tools={['MS SQL', 'NoSQL', 'Oracle', 'Redis', 'DynamoDB', 'Redshift']}>Databases</SkillBar>
-    <SkillBar percentage={97} tools={['Bash', 'AWS', 'Kafka', 'Git', 'CI/CD', 'Serverless', 'Kubernetes']}>Infra</SkillBar>
-    <SkillBar percentage={97} tools={['MSVC', 'MSBuild', 'Windows Driver Foundation']}>Windows</SkillBar>
-    <SkillBar percentage={97} tools={['C/C++', 'Embedded', 'Kernel Modules', 'HID']}>Linux (and friends)</SkillBar>
-    <SkillBar percentage={97} tools={['UE4', 'Unity', 'Godot', 'Networking', 'Shaders']}>Gamedev</SkillBar>
+    <SkillBar percentage={98} tools={['Spring', 'Gradle', 'Hibernate', 'Kotlin', 'Scala']}>Java</SkillBar>
+    <SkillBar percentage={95} tools={['ASP.NET', 'EntityFramework', 'C#', 'Mono']}>.NET</SkillBar>
+    <SkillBar percentage={92} tools={['TypeScript', 'React', 'Angular', 'Sass', 'Svelte', 'Electron']}>Front-End</SkillBar>
+    <SkillBar percentage={88} tools={['MS SQL', 'NoSQL', 'Oracle', 'Redis', 'DynamoDB', 'Redshift']}>Databases</SkillBar>
+    <SkillBar percentage={90} tools={['Bash', 'AWS', 'Kafka', 'Git', 'CI/CD', 'Serverless', 'Kubernetes']}>Infra</SkillBar>
+    <SkillBar percentage={80} tools={['MSVC', 'MSBuild', 'Windows Driver Foundation']}>Windows</SkillBar>
+    <SkillBar percentage={85} tools={['C/C++', 'Embedded', 'Kernel Modules', 'HID']}>Linux (and friends)</SkillBar>
+    <SkillBar percentage={89} tools={['UE4', 'Unity', 'Godot', 'Networking', 'Shaders']}>Gamedev</SkillBar>
     <SkillBar percentage={0} tools={['Agile', 'Debugging', 'JWT', 'GraphQL', 'XSL', 'IAM', 'SSO', 'DevOps']}>Other</SkillBar>
     <!-- TODO for some reason this is clipped in browser but correct in pdf -->
     <div class="claim">This resume built using <a href="https://svelte.dev/">Svelte</a></div>
