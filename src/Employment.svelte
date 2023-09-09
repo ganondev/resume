3ae8fabfa161b29823e827f94f69e78cebc1620f
EJ <me@me>
2023-09-08 21:24:56 -0400

    Update employment items

--- src/Employment.svelte
+++ src/Employment.svelte
@@ -26,21 +26,37 @@
             Develop and maintain the Compute Ops Manager platform
         </Acc>
         <Acc>
-            Scale platform services to support parallel communication with tens
-            of thousands of customer compute devices (iLO)
+            Scale platform services to support asyncronous real-time communication
+            with tens of thousands of customer devices (iLO)
         </Acc>
         <Acc>
-            Support global multi-region production in daily deploy and real-
-            time defect and health triage
+            Independently develop tools to allow on-call developers to efficiently
+            identify and catalogue production and pipeline issues based on log signatures
+        </Acc>
+<!--        <Acc>-->
+<!--            Own stack of React UI, asynchronous and-->
+<!--            synchronous microservice layer, AWS infrastructure, database administration,-->
+<!--            onboarding of customer devices, and the CI/CD process-->
+<!--        </Acc>-->
+        <Acc>
+            Modernize UI with patented state management and UX flow framework systems
+            based on functional and procedural programming paradigms
         </Acc>
         <Acc>
-            Enhance and maintain complete stack of UI, asynchronous and
-            synchronous service layer, AWS infrastructure, database administration,
-            integration of real customer-owned servers, and the CI/CD process
+            Enable customers to generate reports of metrics relevant to their devices,
+            such as carbon emissions, by constructing real-time device data collection
+            and archival infrastructure and microservices
         </Acc>
+<!--        <Acc>-->
+<!--            Enable customer awareness of their carbon footprint via development-->
+<!--            of carbon reporting technology-->
+<!--        </Acc>-->
+        <!--        <Acc>-->
+        <!--            Support global multi-region production in daily deploy and real- -->
+        <!--            time defect and health triage.-->
+        <!--        </Acc>-->
         <Acc>
-            Enable customer awareness of their carbon footprint via development
-            of carbon reporting technology
+            Construct real-time customer incident and notification system
         </Acc>
     </Gig>
     <Gig
@@ -50,26 +66,26 @@
         employer="Vanguard"
     >
         <Acc>Program engineer spotlight: September 2021</Acc>
-        <Acc>Save tens of thousands by optimizing Docker allocations in ECS</Acc>
+        <Acc>Save tens of thousands $ by optimizing Docker runtimes in ECS</Acc>
         <Acc>
             Lead team to develop Electron application and serverless
-            infrastructure to enhance weekly production release cycle
+            infrastructure to enhance continuous production release cycle
         </Acc>
         <Acc>
-            Overhaul architecture of many complex microservices to utilize
-            resilient event-driven patterns
+            Overhaul architecture microservice layer to establish more
+            resilient and scalable event-driven patterns
         </Acc>
         <Acc>
-            Own full stack of Nx Angular UI, hundreds of Java/Springboot microservices,
-            and the AWS resources that power them
+            Own full stack of Nx Angular UI, numerous of Java/Springboot microservices,
+            and the AWS infrastructure that powers them
         </Acc>
         <Acc>
-            Apply Machine Learning and Monte Carlo simulations to calculate
-            and optimize retirement success
+            Apply and train machine learning models and Monte Carlo simulations
+            to predict and optimize retirement success
         </Acc>
         <Acc>
             Implement high-throughput analytics pipelines utilizing
-            Kafka, S3, SageMaker, Lambda, and Redshift
+            Kafka, S3, SageMaker, Lambda, Redshift, Glue, EMR, and Athena
         </Acc>
     </Gig>
     <Gig
@@ -81,45 +97,58 @@
         <Acc>Consultant spotlight: May 2020</Acc>
         <Acc>
             Manage long and short-term engagements, mentoring developers,
-            driving Agile teams, and making architecture decisions
+            driving Agile teams, and constructing software architecture
         </Acc>
         <Acc>
-            Develop APIs that reduced development effort on ongoing
-            projects by 30% and increased software efficiency by up to 41%
+            Work at all levels of the stack to create and improve client
+            portals, microservices, ETL workflows, and CI pipelines
         </Acc>
         <Acc>
-            Work at all levels of the stack to create and improve multiple
-            sets of portals, microservices, and business pipelines
+            Establish confidence through thorough testing systems,
+            including unit, integration, and behavioural via Cypress and Selenium
         </Acc>
         <Acc>
-            Test using JUnit, NUnit, Cypress, and Selenium
+            Efficient contributor who lead struggling team to deliver
+            quality full-stack web application packages in under 10 weeks
         </Acc>
         <Acc>
-            At-all-costs contributor who drove teams to deliver quality full-stack web
-            application packages in under 10 weeks
+            Secure customer applications using AWS IAM, as well as RBAC via
+            manual JWT management with SSO integrations.
         </Acc>
-        <Acc>
-            Secured application packages for businesses and users using AWS
-            IAM, Google Sign-In, as well as manual JWT role authorization.
-        </Acc>
+        <!--
+        TODO embeds:
+        NUnit,
+        Google Sign-In,
+        unit tests, unit testing
+        integration tests, integration testing
+        Kafka,
+        AWS S3,
+        AWS SageMaker,
+        AWS Lambda,
+        AWS Redshift,
+        AWS Glue,
+        AWS EMR,
+        AWS Athena,
+        AWS Kinesis Firehose,
+        Go, Golang,
+        ElasticSearch,
+        OpenSearch,
+        FastAPI, Flask, Django, PyUnit, Extensions API
+        -->
     </Gig>
     <Gig
-        title="Freelance Software Engineer"
+        title="Freelance Developer"
         start="Dec 2016"
         end="Jan 2018"
-        employer="[Various]"
+        employer="Multiple Clients"
     >
-        <Acc>
-            <b>Clients:</b> Society of St. John the Evangelist, Asset Research Group
-            LLC., Virginia Theological Seminary
-        </Acc>
         <Acc>
             Introduce critical automation and future-proofing for
             agencies outgrowing their manpower
         </Acc>
         <Acc>
-            Accelerated the development of global event pages against short
-            deadlines while ensuring highly reliable and scalable products
+            Accelerated the development of products and events by consulting on
+            tools and practices to decrease turn-around times and maintenance costs
         </Acc>
     </Gig>
 </div>
