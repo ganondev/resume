75b9bfbae6d614e578b61b04e7328deedf72933f
EJ <me@me>
2023-09-09 00:37:24 -0400

    PDF generation

---
+++ makepdf.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('http://localhost:5000', { waitUntil: 'networkidle2' });
+
+    await page.pdf({ path: 'resume.pdf', format: 'A4', printBackground: true, pageRanges: '1'});
+
+    await browser.close();
+})();
